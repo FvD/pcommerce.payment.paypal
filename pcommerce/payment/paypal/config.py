5a75fc70aa3faa47c71adad3555de3b9f6e205d7
Steffen Lindner <gomez@flexiabel.de>
2012-11-30 12:00:57 +0100

    Configlet finished

--- pcommerce/payment/paypal/config.py
+++ pcommerce/payment/paypal/config.py
@@ -1,15 +1,8 @@
     
     
-CONFIGLET_PROPERTIES = (dict(name='paypal_receiver_email', type_='string',value='',required=True),
-                        dict(name='api_username', type_='string',value='',required=True),
+CONFIGLET_PROPERTIES = (dict(name='api_username', type_='string',value='',required=True),
                         dict(name='api_password',type_='string',value='',required=True),
                         dict(name='api_signature',type_='string',value='',required=True),
-                        dict(name='ipn_notify_url',type_='string',value='',required=True),
-                        dict(name='receiver_id',type_='string',value='',required=True),
-                        dict(name='test', type_='boolean',value=False,required=False),
-                        dict(name='test_paypal_personal_email',type_='string',value='',required=False),
-                        dict(name='test_visa_account_no',type_='string', value='',required=False),
-                        dict(name='test_visa_expiration',type_='string', value='',required=False),)
-                       
-
-
+                        dict(name='ipn_notify_url',type_='string',value='',required=False),
+                        dict(name='receiver_id',type_='string',value='',required=False),
+                        dict(name='test', type_='boolean',value=False,required=False),)
